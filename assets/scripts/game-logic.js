1811ad5ac2d8fa8d3f7c008201e6176cfb7f6142
cpstrillvt93 <cpsvt93@gmail.com>
2017-01-28 14:40:48 -0500

    logic getting there

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -2,33 +2,49 @@
 
 // global variables
 let board = ['', '', '', '', '', '', '', '', ''];
-let player = 'x';
 
+let player = 'X';
+//look through board for
 //switch between x and o function
-let player_turn = function () {
-  if(player === 'x') {
-    player = 'o';
+// let switchPlayer = function(index) {
+//   let countPlayer = 0;
+//   for (let i = 0;) {
+//     if (board[i] === "X" || board[i] === "O"){
+//     countPlayer++;
+//   }
+// };
+// };
+let switchPlayer = function () {
+  if (player === 'X') {
+    player = 'O';
   }
   else {
-    player = 'x';
+    player = 'X';
   }
   console.log(player + '' + 'Your turn!');
 };
 
 //play x or o depending on player_turn;
-const play_move = function(player){
-  $('.tiles').text(player);
+let play_move = function(index){
+  if(board[index] !== "X" && board[index] !== "O"){
+    if (player === 'X') {
+      board[index] = 'X';
+    } else {
+      board[index] = 'O';
+    }
+  }
+
   console.log('nice move');
 };
 
 //create game board that puts value in correct game board/tile index
 // one of the two below?
 
-let setGameArray = function (tiles) {
-  let index = $(tiles).data('.board');
-  board[index] = player;
-  console.log(board);
-};
+// let setGameArray = function (tiles) {
+//   let index = $(tiles).data('.board');
+//   board[index] = player;
+//   console.log(board);
+// };
 
 // let setGameArray = function (board) {
 //   let index = $(this).parent().index('.board');
@@ -46,16 +62,12 @@ let diagonalWinner = function (board) {
     console.log('You Win');
     return true;
   }
-  else if (board[6] !== '' && board[6] === board[7] && board[7] === board[8]) {
-    console.log('You Win');
-    return true;
-  }
   else {
     return false;
   }
 };
 
-let rowWinner = function () {
+let rowWinner = function (board) {
   if (board[0] !== '' && board[0] === board[1] && board[0] === board[2]){
     console.log('You Win!');//Jquery?
     return true;
@@ -74,52 +86,75 @@ let rowWinner = function () {
 };
 
 let columnWinner = function (board) {
-  if (board[0] !== '' && board[0] === board[3] && board[0] === board[6]){
+  if ((board[0] !== '' && board[0] === board[3] && board[0] === board[6]) ||
+  (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) ||
+  (board[2] !== '' && board[2] === board[5] && board[2] === board[8])) {
     console.log('You Win!');//Jquery?
     return true;
   }
-  else if (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) {
-    console.log('You Win');
-    return true;
-  }
-  else if (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) {
-    console.log('You Win');
-    return true;
-  }
-  else {
-    return false;
-  }
+  // else if (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) {
+  //   console.log('You Win');
+  //   return true;
+  // }
+  // else if (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) {
+  //   console.log('You Win');
+  //   return true;
+  // }
+  // else {
+  //   return false;
+  // }
 };
+
+
 //combine all winning conditional functions for one exportable function.
 const winner = function () {
   if(diagonalWinner === true || rowWinner === true || columnWinner === true) {
-    return true;
-  }
-  console.log(player + '' + 'you win!');
+  console.log('player ' + player + ' ' + board + 'you win!');
+}
 };
 //Make tile click with appropriate function
-const onTileClick = function (event) {
-  if ($('.tiles').text() === '') {
-    $( ".tiles" ).text(player);
+const updateBoard = function () {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === 'X') {
+      $('#' + i).text('X');
+    } else if (board[i] === 'O') {
+      $('#' + i).text('O');
+    }
   }
-  else {
-    alert('error');
-  }
-    play_move(event.target);
-    setGameArray(event.target);
-    player_turn();
-    winner();
 };
 
+
+const runGame = function (event) {
+  switchPlayer();
+  play_move(parseInt(event.target.id));
+  console.log(board);
+  updateBoard();
+  winner();
+};
+
+  //if ($( !== X OR O))
+
+    // // play_move();
+    // setGameArray();
+    // winner();
+// };
 const addHandlers = () => {
-  $('.tiles').on('click', onTileClick);
+  $('#0').on('click', runGame);
+  $('#1').on('click', runGame);
+  $('#2').on('click', runGame);
+  $('#3').on('click', runGame);
+  $('#4').on('click', runGame);
+  $('#5').on('click', runGame);
+  $('#6').on('click', runGame);
+  $('#7').on('click', runGame);
+  $('#8').on('click', runGame);
 };
 module.exports = {
     addHandlers,
     board,
-    onTileClick,
-    player_turn,
+    updateBoard,
+    switchPlayer,
     play_move,
-    setGameArray,
+    runGame,
     winner,
 };
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,17 +5,18 @@ const config = require('./config');
 const game = require('./game-logic');
 $(() => {
   setAPIOrigin(location, config);
+  game.addHandlers();
 });
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
-$(() => {
-  $('.tiles').on('click', game.onTileClick);
+// $(() => {
+
   //
 
   //$('#box-id').on('click',bookEvents.onGetBooks);
 
-});
+// });
 
 // use require without a reference to ensure a file is bundled
 require('./example');
